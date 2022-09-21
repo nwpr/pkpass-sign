948754ea83283b44955b3d523f07880b7c9342f4
Nikolaus Walther <nikolaus@walther.eu.org>
2022-09-21 14:36:26 +0200

    working

--- sign.py
+++ sign.py
@@ -8,56 +8,57 @@ from zipfile import ZipFile, ZIP_DEFLATED
 
 
 class PkpassSigner:
-    KEYPATH = None
-    KEY_FILE = None
-    WWDR_FILE = None
+    _m = None
 
     def __init__(self, cert, key, wwdr):
-        self.cert = m2.X509.load_cert(cert)
-        self.key = m2.EVP.load_key(key)
-        self.wwdr = m2.X509.load_cert(wwdr)
+        self._m = m2.SMIME.SMIME()
+        cs = m2.X509.X509_Stack()
+        cs.push(m2.X509.load_cert(wwdr))
+
+        self._m.load_key(key, cert)
+        self._m.set_x509_stack(cs)
 
     def sign_folder(self, folder):
-        data = io.BytesIO()
-        zip = ZipFile(data, 'w', ZIP_DEFLATED)
-        for f in glob(os.path.join(folder, '*')):
-            zip.write(f, os.path.relpath(f, folder))
+        with io.BytesIO() as data:
+            with ZipFile(data, 'w', ZIP_DEFLATED) as r:
+                for f in glob(os.path.join(folder, '**'), recursive=True):
+                    if not os.path.isfile(f) or os.path.relpath(f, folder) in ['signature', 'manifest.json']:
+                        continue
+                    r.write(f, os.path.relpath(f, folder))
+                manifest = self.create_manifest(r)
+                r.writestr('manifest.json', manifest)
+                r.writestr('signature', self.create_signature(manifest))
+            return data.getvalue()
 
 
     def sign_pkpass(self, pkpass):
-        with ZipFile(pkpass, 'a', ZIP_DEFLATED) as z:
-            manifest = self.create_manifest(z)
-            z.writestr('manifest.json', manifest.read())
-            z.writestr('signature', self.create_signature(manifest).read())
+        with io.BytesIO() as result:
+            with ZipFile(pkpass, 'a', ZIP_DEFLATED) as z:
+                with ZipFile(result, 'w', ZIP_DEFLATED) as r:
+                    for f in z.infolist():
+                        if f.filename in ['signature', 'manifest.json']:
+                            continue
+                        r.writestr(f.filename, z.read(f))
+                    manifest = self.create_manifest(r)
+                    r.writestr('manifest.json', manifest)
+                    r.writestr('signature', self.create_signature(manifest))
+            return result.getvalue()
 
     def create_manifest(self, zipfile):
         manifest = {}
         for f in zipfile.infolist():
-            if f.is_dir():
+            if f.is_dir() or f.filename in ['signature', 'manifest.json']:
                 continue
-            with zipfile.open(f, 'rb') as z:
+            with zipfile.open(f, 'r') as z:
                 manifest[f.filename] = sha1(z.read()).hexdigest()
-        mjson = json.dumps(manifest, indent=1)
-        bytes = io.BytesIO()
-        bytes.write(mjson)
-        return bytes
+        mjson = json.dumps(manifest, indent=1).encode('utf-8')
+        return mjson
 
     def create_signature(self, file):
-        m = m2.SMIME.SMIME()
-        cs = m2.X509.X509_Stack()
-        wwdr = open(os.path.join(self.KEYPATH, self.WWDR_FILE), 'rb').read()
-        cs.push(m2.X509.load_cert_string(wwdr))
-
-        m.load_key(os.path.join(self.KEYPATH, self.KEY_FILE))
-        m.set_x509_stack(cs)
-
         mb = m2.BIO.MemoryBuffer(file)
 
-        pkcs7 = m.sign(mb, m2.SMIME.PKCS7_DETACHED | m2.SMIME.PKCS7_BINARY)
+        pkcs7 = self._m.sign(mb, m2.SMIME.PKCS7_DETACHED | m2.SMIME.PKCS7_BINARY)
 
         mb = m2.BIO.MemoryBuffer()
         pkcs7.write_der(mb)
-        w = io.BytesIO()
-        w.write(mb.read_all())
-        w.close()
-        return w
+        return mb.read_all()
